1e8a63458e05d63a30ad099409adb90dbab9fb99
TylerLu <admin@tyler.lu>
2018-01-09 13:29:51 +0800

    Add code to debug

--- jenkins/add-docker-build-job.sh
+++ jenkins/add-docker-build-job.sh
@@ -214,6 +214,10 @@ done
 #wait for instance to be back online
 run_util_script "jenkins/run-cli-command.sh" -j "$jenkins_url" -ju "$jenkins_username" -jp "$jenkins_password" -c "version"
 
+#show content of files
+cat credentials.xml
+cat job.xml
+
 #add user/pwd
 echo "${credentials_xml}" > credentials.xml
 run_util_script "jenkins/run-cli-command.sh" -j "$jenkins_url" -ju "$jenkins_username" -jp "$jenkins_password" -c 'create-credentials-by-xml SystemCredentialsProvider::SystemContextResolver::jenkins (global)' -cif "credentials.xml"
