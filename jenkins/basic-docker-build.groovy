79b04a7d9ccf8021d21f035ed3e331edaf65ad6e
TylerLu <admin@tyler.lu>
2018-01-10 12:49:50 +0800

    Update environment name to TAGGED_IMAGE_NAME

--- jenkins/basic-docker-build.groovy
+++ jenkins/basic-docker-build.groovy
@@ -14,7 +14,7 @@ node {
       }
     }
     stage('Deploy configurations to Azure Container Service (AKS)') {
-      withEnv(['IMAGE_NAME=' + taggedImageName]) {
+      withEnv(['TAGGED_IMAGE_NAME=' + taggedImageName]) {
         acsDeploy azureCredentialsId: params.azure_service_principal_id, configFilePaths: 'kubernetes/*.yaml', containerService: params.aks_cluster_name + ' | AKS', dcosDockerCredentialsPath: '', enableConfigSubstitution: true, resourceGroupName: params.aks_resource_group_name, secretName: '', sshCredentialsId: ''
       }
     }
