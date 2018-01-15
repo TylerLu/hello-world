71f3fce910f2183b0b27d25916834c4a610e2a17
TylerLu <admin@tyler.lu>
2018-01-15 21:06:56 +0800

    install azure-cli

--- grafana/configure-grafana.sh
+++ grafana/configure-grafana.sh
@@ -88,7 +88,11 @@ function post_json() {
 }
 
 #install azure-cli
-sudo apt-get install azure-cli --yes
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | \
+     sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
+sudo apt-get install apt-transport-https
+sudo apt-get update && sudo apt-get install azure-cli
 
 #get vitrual machines
 az login --service-principal -u $CLIENT_ID --password $CLIENT_SECRET --tenant $TENANT_ID
